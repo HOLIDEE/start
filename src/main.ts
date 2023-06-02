38d2b5a988fce6257273cff0600d3eea24e90a2c
eneyret <manu@holidee.fr>
2023-06-02 17:06:38 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,54 +6,16 @@ console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 
-/**
- * Ennum of button type
- */
-const ActionBarButtonType = {
-    button: "button",
-    action: "action",
-} as const;
-type ActionBarButtonType = typeof ActionBarButtonType[keyof typeof ActionBarButtonType];
-
-interface AddButtonActionBar {
-    /*
-    *   the id of the button action bar defined.
-    */
-    id: string,
-
-    /*
-    *   the label to display in button action bar.
-    */
-    label: string
-
-    /*
-    *   the type of button ('button' / 'action'). By default is 'button'.
-    */
-    type: ActionBarButtonType,
-
-    /*
-    *  the image of button associated, This parameter is nullable.
-    */
-    imageSrc: string
-
-    /*
-    *   the label displayed above the action button. This parameter is nullable.
-    */
-    toolTip: string
-};
-
-WA.ui.actionBar.addButton(descriptor: {
-    id: string,
-    type: 'action',
-    imageSrc: string,
-    toolTip: string,
-}):
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
+interface PlayerVariableChanged {
+    player: RemotePlayer;
+    value: unknown;
+}
+WA.players.onVariableChange(variableName: string): Observable<PlayerVariableChanged>
 // Action zone "visit"
 	WA.room.area.onEnter('Arena').subscribe(() => {
 		WA.ui.modal.openModal({
